0c37b81bf24d258ac865defdec8d9cb37904408d
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-10-13 09:07:56 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,11 @@
+parent.postMessage(window.location.toString(), "*");
+
+/* Override window.alert */
+var originalAlert = window.alert;
+window.alert = function(s) {
+  parent.postMessage("success", "*");
+  setTimeout(function() { 
+    originalAlert("Congratulations, you executed an alert:\n\n" 
+      + s + "\n\nYou can now advance to the next level.");
+  }, 50);
+}
